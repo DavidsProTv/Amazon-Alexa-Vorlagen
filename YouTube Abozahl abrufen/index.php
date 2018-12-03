c314f28fa7c857f436f7bda87c3809e5cbb7532e
DavidsProTv <32435357+DavidsProTv@users.noreply.github.com>
2018-12-03 16:36:27 +0100

    Create index.php

---
+++ YouTube Abozahl abrufen/index.php
@@ -0,0 +1,68 @@
+<?php
+// Alexas Aufforderung wird entgegengenommen
+$jsonRequest = file_get_contents('php://input');
+// Alexas Aufforderung wird entschlüsselt
+$data        = json_decode($jsonRequest, true);
+// Bei einerm Fehlendem Request von Alexa wird das ausgegeben.
+if( empty($data) || (!isset($data) ) ) {
+	die('Bad Request');
+}
+
+
+
+//Falls kein Intent exestiert wird default aufgerufen
+$intent = !empty($data['request']['intent']['name']) ? $data['request']['intent']['name'] : 'default';
+switch ($intent) {
+	case "Kanal2":
+	$channelID = "####KANALID####";
+	require("getabozahl.php");
+		$text = "Du hast derzeit ".$abozahl." Abonnenten";
+		$responseArray = [ 'version' => '1.0',
+				'response' => [
+							'outputSpeech' => [
+										'type' => 'PlainText',
+										'text' => $text,
+										'ssml' => null
+							],
+							'shouldEndSession' => false
+				]
+		];
+		break;
+		case "Kanal1":
+		$channelID = "####KANALID####";
+	  require("getabozahl.php");
+	    $text = "Du hast derzeit ".$abozahl." Abonnenten";
+			$responseArray = [ 'version' => '1.0',
+			    'response' => [
+			          'outputSpeech' => [
+			                'type' => 'PlainText',
+			                'text' => $text,
+			                'ssml' => null
+			          ],
+	              'shouldEndSession' => false
+			    ]
+			];
+			break;
+	default:
+	$channelID = "####KANALID####";
+  require("getabozahl.php");
+    $text = "Du hast derzeit ".$abozahl." Abonnenten";
+		$responseArray = [ 'version' => '1.0',
+		    'response' => [
+		          'outputSpeech' => [
+		                'type' => 'PlainText',
+		                'text' => $text,
+		                'ssml' => null
+		          ],
+              'shouldEndSession' => false
+		    ]
+		];
+		break;
+}
+//Macht die Datei zu einer Json Datei
+header ( 'Content-Type: application/json' );
+//Gibt den Code aus
+echo json_encode ( $responseArray );
+//beendet die Datei
+die();
+?>
